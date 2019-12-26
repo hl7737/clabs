6d4227d2556ba2e8433c7581fca730a8cd5f47ee
hl7737 <850567274@qq.com>
2019-12-26 04:15:57 -0800

    added a file

--- lab3/ex3.c
+++ lab3/ex3.c
@@ -1,36 +1,54 @@
 #include<stdio.h>
 #include<math.h>
+int power(int base, int n){
+    int i,p;
+    p=1;
+    for (i=1;i<=n;++i)
+    p = p*base;
+    return p;
+}
 int main()
 {
-	char a[20];
-	int b[20] = { 0 };
-	int i = 0, j = 0;
-	int num = 0;
-	int c = 0;
-	while (a[num] != '\0'){ 
-	if ((a[num] >= 'a') && (a[num] <= 'z'))
-		{
-			b[num] = a[num] - 'a' + 10;
-			num++;
-			continue;
-		}
-		if ((a[num] >= 'A') && (a[num] <= 'Z'))
-		{
-			b[num = a[num] - 'A' + 10];
-			num++;
-			continue;
-		}
-		b[num] = a[num] - '0';
-		num++;
-	}
-	for (i = 0; i < num; i++)
-	{
-		b[num - i - 1] = b[num - i - 1] * pow(16, i);
-	}
-	for (j = 0; j < num; j++)
-	{
-		c = c + b[j];
-	}
-	printf("%d", c);
-	return 0;
+    char number1[18] = {0};
+    int i = 0;
+    char number;
+    int sum = 0;
+    int gen =-1;
+    unsigned int b = 1;
+    printf("please input a positive hexadecimal number(start with 0X/0x)\n");
+    while((number = getchar()) != '\n'){
+        number1[i]=number;
+        i++;
+        gen++;
+    }
+        if(number1[0]!='0'){
+        printf("input error\n");
+        return 0;}
+        if(number1[1]!='x'&&number1[1]!='X'){
+            printf("input error\n");
+            return 0;
+        }
+        i--;
+        while(i > 1)
+    {
+            if(number1[i]<='9'&&number1[i]>='0')
+                sum+=(number1[i]-'0')*power(16,gen-i);
+            else if(number1[i]>='A'&&number1[i]<='F')
+                sum+=(number1[i]-'A'+10)*power(16,gen-i);
+            else if(number1[i]>='a'&&number1[i]<='f')
+            sum+=(number1[i]+'A'-'a'-'A'+10)*power(16,gen-i);
+            else{
+                printf("input error\n");
+                return 0;
+                }
+            i--;
+    }
+    while(b!=0){
+        b=b<<1;
+    }
+    if(sum>b-1){
+    printf("the max is %u,your input number  is too big\n",b-1);
+    }
+    else printf("%d\n",sum);
+    return 0 ;
 }
