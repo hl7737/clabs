f6e3c59a3332739f50f6552a4e073acc0fa87442
hl7737 <850567274@qq.com>
2019-12-26 05:04:49 -0800

    added a file

--- lab3/ex3.1.c
+++ lab3/ex3.1.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 #include<string.h>
+
 int strindex (char s[], char t[])
 {
-    int i,j,k,c,pos=0;
+    int i,j,k,pos=0;
     for(i = 0;s[i] !='\0'; i++){
         for(j = i,k=0;t[k]!='\0'&& s[j]==t[k];j++,k++);
-        if(t[k]=='\0'){
+        if(t[k]=='\0'&& k>0){
         pos = i+1;
-        c=k;
     }
     }
     if(pos >0)
-    return (pos-1+c-1);
+    return pos-1;
     return -1;
 }
 int main(){
