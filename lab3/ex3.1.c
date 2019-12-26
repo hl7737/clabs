ccc1fce864f4aa10199c5b5667d5b8d26eda5bcc
hl7737 <850567274@qq.com>
2019-12-26 04:08:20 -0800

    added a file

--- lab3/ex3.1.c
+++ lab3/ex3.1.c
@@ -1,11 +1,23 @@
-#include <stdio.h>
-int strrindex(char s[],char t[]);
+#include<stdio.h>
+#include<string.h>
+int strindex (char s[], char t[])
 {
-    int i,j,m,num;
-    for (i=0;s[i]!='\0';i++){
-    for (j=i;m=0;t[m]!='\0' && s[j]==t[m];j++;m++)
-    if(m>0 && t[m]=='\0')
-    num=i;
+    int i,j,k,c,pos=0;
+    for(i = 0;s[i] !='\0'; i++){
+        for(j = i,k=0;t[k]!='\0'&& s[j]==t[k];j++,k++);
+        if(t[k]=='\0'){
+        pos = i+1;
+        c=k;
     }
-    return num;
+    }
+    if(pos >0)
+    return (pos-1+c-1);
+    return -1;
+}
+int main(){
+    char a[100];
+    char b[100];
+    printf("please input the chain of characters\n");
+    scanf("%s %s", a,b);
+    printf("%d\n",strindex(b,a));
 }
