7bc47cd0f457e467b7d9e1e0cfef9d5306ced66e
hl7737 <850567274@qq.com>
2019-12-26 03:49:01 -0800

    added a file

--- lab5/5-2.c
+++ lab5/5-2.c
@@ -1,23 +1,71 @@
 #include <stdio.h>
 #include <ctype.h>
-int getch(void);
-void ungetch(int);
-int getint(float *pn)
+char buf[100];
+int bufp = 0;
+int getch(void)
 {
-    int c,sign;
-    while (isspace(c=getch()));
-    if (!isdigit(c) && c != EOF && c != '+' && c != '-'){
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+void ungetch(int c)
+{
+    if (bufp >= 100)
+        printf("full");
+    else
+        buf[bufp++] = c;
+}
+int getfloat(float *pn)
+{
+    int c, sign;
+    float i;
+    while (isspace(c = getch())&&c!='\n')
+        ;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-' && c != '.')
+    {
         ungetch(c);
         return 0;
     }
     sign = (c == '-') ? -1 : 1;
     if (c == '+' || c == '-')
         c = getch();
-    for (*pn = 0;isdigit(c);c = getch())
-         *pn = 10 * *pn + (c - '0');
+    int flag = 0;
+    for (*pn = 0.0, i = 1; isdigit(c) || c == '.'; c = getch())
+    {
+        if (c == '.')
+            flag = 1;
+        else if (flag == 1)
+        {
+            i *= 0.1;
+            *pn = *pn + (c - '0') * i;
+        }
+        else if (isdigit(c))
+            *pn = 10.0 * *pn + (c - '0');
+    }
     *pn *= sign;
-    if (c != EOF)
+    if (c != '\n')
         ungetch(c);
-        printf("总数为:%f\n",sum);
-    return c;
+    else
+    {
+        ungetch(c);
+    }
+    return *pn;
+}
+int main()
+{
+    float a[100];
+    int n = 0;
+    float sum = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        getfloat(&a[i]);
+        if (a[i] != '\0')
+        {
+            n++;
+            sum += a[i];
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("%g\n", sum);
 }
